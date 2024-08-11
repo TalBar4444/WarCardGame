79beb1942d620e923e504a1726e645e9571e37a7
TalBar4444 <talbar458@gmail.com>
2024-08-11 22:22:47 +0300

    added GameController first view

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,18 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var main_LBL_title: UILabel!
-
-    @IBOutlet weak var main_BTN_send: UIButton!
+    @IBOutlet weak var main_LBL_start: UIButton!
     
+    @IBOutlet weak var main_LBL_name: UILabel!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func sendClicked(_ sender: UIButton) {
-        main_BTN_send.setTitleText(text: "Sending")
-        main_LBL_title.text = "Hello World"
+//    @IBAction func sendClicked(_ sender: UIButton) {
+//        main_BTN_send.setTitleText(text: "Sending")
+//        main_LBL_title.text = "Hello World"
+//    }
+    @IBAction func openMainActivity(_ sender: UIButton) {
+        main_LBL_name.text = "Hello Tal"
     }
     
 }
