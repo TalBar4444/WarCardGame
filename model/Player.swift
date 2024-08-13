c57d7f54917c61e8b58453bcbca561db8d2fbb9f
Tal Bar <130868710+TalBar4444@users.noreply.github.com>
2024-08-13 17:20:40 +0300

    Add files via upload

---
+++ model/Player.swift
@@ -0,0 +1,48 @@
+//
+//  Player.swift
+//  WarCardGame
+//
+//  Created by Student31 on 12/08/2024.
+//
+
+import Foundation
+
+class Player{
+    var name: String
+    var score: Int
+    var cards: [Card]
+    
+    
+    init(name: String, score: Int, cards: [Card]){
+        self.name = name
+        self.score = score
+        self.cards = cards
+        self.cards.shuffle()
+    }
+    
+    func getCard() -> Card{
+        let card = self.cards.removeFirst()
+        return card
+    }
+    
+    func addCards(cards: [Card]){
+        self.cards.append(contentsOf: cards)
+    }
+    
+    func getCardName() -> String{
+        return self.cards[0].getImage()
+    }
+    
+    func getScore() -> Int{
+        return self.score
+    }
+    
+    func getCardScore() -> Int{
+        return self.cards[0].value
+    }
+    
+    func setName(name: String){
+        self.name = name
+    }
+    
+}
